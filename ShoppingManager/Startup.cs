be76ab2dcad0c3b283b160a2510e4c31bee76578
Timur <kashapov11im@mail.ru>
2020-03-12 20:25:01 +0300

    Добавил бизнес контроллер

--- ShoppingManager/Startup.cs
+++ ShoppingManager/Startup.cs
@@ -16,25 +16,44 @@ namespace ShoppingManager
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
 		public void ConfigureServices(IServiceCollection services)
 		{
+			
+			// установка конфигурации подключения
+
+			services.AddMvc(option => option.EnableEndpointRouting = false);
+
+			services.AddMvc();
+			
+		
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseCors();
+			app.UseDefaultFiles();
+
+
+			app.UseStaticFiles();
+			app.UseAuthentication();
+
+			app.UseMvc(routes =>
+			{
+				routes.MapRoute(
+					name: "default",
+					template: "{controller=Home}/{action=Index}/{id?}");
+			});
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
 			}
-
-			app.UseRouting();
-
-			app.UseEndpoints(endpoints =>
+			else
 			{
-				endpoints.MapGet("/", async context =>
-			  {
-				    await context.Response.WriteAsync("Hello World!");
-			    });
-			});
+				app.UseHsts();
+			}
+
+			app.UseHttpsRedirection();
+			app.UseMvc();
 		}
 	}
 }
