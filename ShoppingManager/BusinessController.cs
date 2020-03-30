d445e53daad1113a7b933c3cca7cf9f029f2609d
Timur <kashapov11im@mail.ru>
2020-03-30 13:55:06 +0300

    чтобы не сломать ничего

--- ShoppingManager/BusinessController.cs
+++ ShoppingManager/BusinessController.cs
@@ -3,16 +3,33 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+using System.Web.Http;
+using HttpGetAttribute = Microsoft.AspNetCore.Mvc.HttpGetAttribute;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute;
 
 namespace ShoppingManager
 {
+    [ApiController]
     [Route("/api/[controller]")]
     public class BusinessController : Controller
     {
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult> Get()
         {
-            return "Привет!";
+            //return Ok("Привет!");
+            return BadRequest();
+            //return "Привет!";
+            //return null;
         }
+
+        //[HttpGet]
+        //public string Get()
+        //{
+        //    return Ok("Привет!");
+        //    return "Привет!";
+        //    return null;
+        //}
     }
 }
--- ShoppingManager/Startup.cs
+++ ShoppingManager/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace ShoppingManager
 {
@@ -22,8 +23,16 @@ namespace ShoppingManager
 			services.AddMvc(option => option.EnableEndpointRouting = false);
 
 			services.AddMvc();
-			
-		
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+				{
+                    Version = "v1",
+                    Title = "Test API",
+                    Description = "ASP.NET Core Web API"
+                });
+            });
+
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +63,11 @@ namespace ShoppingManager
 
 			app.UseHttpsRedirection();
 			app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
+            });
 		}
 	}
 }
