62fe966e0d61459d49a3b08f580fec3cbbac1a47
Timur <kashapov11im@mail.ru>
2020-04-01 15:14:07 +0300

    Создали базу данных

--- ShoppingManager/Startup.cs
+++ ShoppingManager/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace ShoppingManager
 {
@@ -15,6 +17,11 @@ namespace ShoppingManager
 	{
 		// This method gets called by the runtime. Use this method to add services to the container.
 		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+		public IConfiguration Configuration	{	get;}
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
 		public void ConfigureServices(IServiceCollection services)
 		{
 			
@@ -32,7 +39,8 @@ namespace ShoppingManager
                     Description = "ASP.NET Core Web API"
                 });
             });
-
+			var con = Configuration["ConnectionString"];
+			services.AddDbContext<ProductContext>(options => options.UseSqlServer(con));
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
