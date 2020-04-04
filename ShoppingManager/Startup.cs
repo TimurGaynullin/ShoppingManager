b2c3efe1319850a6bb2ae6631c88d6a323b70345
Timur <t.cornet@yandex.ru>
2020-04-04 14:54:40 +0300

    gitignore

--- ShoppingManager/Startup.cs
+++ ShoppingManager/Startup.cs
@@ -1,82 +1,70 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 
 namespace ShoppingManager
 {
-	public class Startup
-	{
-		// This method gets called by the runtime. Use this method to add services to the container.
-		// For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-		public IConfiguration Configuration	{	get;}
-		public Startup(IConfiguration configuration)
-		{
-			Configuration = configuration;
-		}
-		public void ConfigureServices(IServiceCollection services)
-		{
-			
-			// установка конфигурации подключения
+    public class Startup
+    {
+        IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
 
-			services.AddMvc(option => option.EnableEndpointRouting = false);
-
-			services.AddMvc();
+            services.AddMvc();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
-				{
+                {
                     Version = "v1",
                     Title = "Test API",
                     Description = "ASP.NET Core Web API"
                 });
             });
-			var con = Configuration["ConnectionString"];
-			services.AddDbContext<ProductContext>(options => options.UseSqlServer(con));
-		}
+            var con = Configuration["ConnectionString"];
+            services.AddDbContext<ProductContext>(options => options.UseSqlServer(con));
+        }
 
-		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-		{
-			app.UseCors();
-			app.UseDefaultFiles();
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseCors();
+            app.UseDefaultFiles();
 
 
-			app.UseStaticFiles();
-			app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseAuthentication();
 
-			app.UseMvc(routes =>
-			{
-				routes.MapRoute(
-					name: "default",
-					template: "{controller=Home}/{action=Index}/{id?}");
-			});
-            //???? ?????????? ? ???????? ??????????
-			if (env.IsDevelopment())
-			{
-                // ?? ??????? ?????????? ?? ?????? ??? ?? ???????
-				app.UseDeveloperExceptionPage();
-			}
-			else
-			{
-				app.UseHsts();
-			}
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
-			app.UseHttpsRedirection();
-			app.UseMvc();
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Test API V1");
             });
-		}
-	}
+        }
+    }
 }
